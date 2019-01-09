0b996b81d5f7c654a04479313de965b0b6b47fbb
294321259@qq.com <Casually123>
2019-01-09 09:50:29 +0800

    1、修改自动签到，解决由于文件过大，签到账号过多导致长时间占用文件致使服务器资源使用不足错误

--- src/main/java/cc/casually/singin/jq22/SingInAll.java
+++ src/main/java/cc/casually/singin/jq22/SingInAll.java
@@ -8,15 +8,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * 签到
  *
  * @author Casually5
  */
-public class SingInAll extends Thread{
+public class SingInAll extends Thread {
     @Override
     public void run() {
         try {
@@ -27,15 +29,11 @@ public class SingInAll extends Thread{
     }
 
     private void singinAll() throws IOException {
-        String dataPath = "D:/jq22.txt";
-        dataPath = System.getProperty("user.dir") + "/data/jq22.txt";
-        FileReader file = new FileReader(dataPath);
-        BufferedReader bf = new BufferedReader(file);
-        String line = "";
+        List<String> datas = readData();
         String[] arrs = new String[2];
-        FileUtil.writeFile( System.getProperty("user.dir") + "/data/result.txt",
+        FileUtil.writeFile(System.getProperty("user.dir") + "/data/result.txt",
                 new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        while ((line = bf.readLine()) != null) {
+        for (String line : datas) {
             line = line.replaceAll(" ", "");
             if (line.startsWith("#")) {
                 continue;
@@ -55,12 +53,12 @@ public class SingInAll extends Thread{
                 /** 获取最终签到多少天 */
                 String endStr = new Nodes(gainSignInParam.reqUrl(cookie).getBodyStr()).getListNodeForTag("h4").get(0).getText();
                 System.out.println(startStr + "===" + endStr);
-                FileUtil.writeFile( System.getProperty("user.dir") + "/data/result.txt",
+                FileUtil.writeFile(System.getProperty("user.dir") + "/data/result.txt",
                         arrs[0] + "：" + startStr + "===" + endStr);
-                if (startStr.equals(endStr)){
-                    System.out.println(String.format("账号：%s签到失败",arrs[0]));
-                }else{
-                    System.out.println(String.format("账号：%s签到成功",arrs[0]));
+                if (startStr.equals(endStr)) {
+                    System.out.println(String.format("账号：%s签到失败", arrs[0]));
+                } else {
+                    System.out.println(String.format("账号：%s签到成功", arrs[0]));
                 }
                 /** 签到完成后退出登录的账号，避免后续账号无法正确登录 */
                 new SingOut().singOut(cookie);
@@ -69,12 +67,24 @@ public class SingInAll extends Thread{
             }
 
             try {
-                /** 没签到完成一个都休眠一分钟，避免过多的请求，导致无法正确签到 */
+                /** 每签到完成一个都休眠一分钟，避免过多的请求，导致无法正确签到 */
                 sleep(60 * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         System.out.println("签到完成");
+    }
+
+    public List<String> readData() throws IOException {
+        List<String> datas = new ArrayList<>(16);
+        String dataPath = System.getProperty("user.dir") + "/data/jq22.txt";
+        FileReader file = new FileReader(dataPath);
+        BufferedReader bf = new BufferedReader(file);
+        String line = "";
+        while ((line = bf.readLine()) != null) {
+            datas.add(line);
+        }
+        return datas;
     }
 }
